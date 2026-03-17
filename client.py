1cef3bcf7414b20d2dfc52e91e7a5d17092533a1
jhDrech <jdrechmer@gmail.com>
2026-03-17 17:03:14 -0300

    .

--- client.py
+++ client.py
@@ -1,12 +1,14 @@
 # Cliente
 import socket
 
-HOST = "192.168.246.31"
-PORT = 9002
+HOST = "192.168.248.114"
+PORT = 9007
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    while True:
-        msg = input()
-        s.sendall(msg.encode('utf-8'))
-        print(s.recv(1024).decode())
+    print(s.recv(1024).decode())
+    print(s.recv(1024).decode())
+    print(s.recv(1024).decode())
+    msg = input()
+    s.sendall(msg.encode('utf-8'))
+    print(s.recv(1024).decode())
--- server.py
+++ server.py
@@ -5,12 +5,33 @@ HOST = "0.0.0.0"
 PORT = 9002
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    while True:
-        s.listen(1)
-        conn, addr = s.accept()
-        print("Cliente:", addr)
-        with conn:
-            data = conn.recv(1024)
-            conn.sendall(b"OK: " + data)
-            print("desconectado")
+    s.bind((HOST, PORT)) #pede a liberação
+    s.listen(1) #começa a usar
+
+    print("[server] aguardando jogador1")
+    conn, addr = s.accept() #ponto de sincronização
+    print("[server] aguardando jogador2")
+    conn2, addr2 = s.accept()
+            
+    conn.sendall("[server] OK. Você é o jogador 1".encode())
+    conn2.sendall("[server] OK. Você é o jogador 2".encode())
+
+    ganhador = None
+
+    msg1 = s.recv(1024).decode()
+    print("jogador1 jogou", msg1)
+    msg2 = s.recv(1024).decode()
+    print("jogador2 jogou", msg2)
+
+    if(msg1 == msg2):
+        print("empate")
+    elif(msg1 == "papel"):
+        if(msg2 == "pedra"):
+            
+        if(msg2 == "tesoura"):
+
+
+
+
+    conn.close()
+    conn2.close()
